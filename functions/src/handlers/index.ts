48aff660e91dcb4304d3436499fe9a1ff2f4a227
jon ceanfaglione <jon@>
2022-11-09 13:10:08 -0500

    start crawler

--- functions/src/handlers/index.ts
+++ functions/src/handlers/index.ts
@@ -1,19 +1,36 @@
 import { Context, S3Event } from 'aws-lambda';
+import { StartCrawlerRequest, StartCrawlerResponse} from 'aws-sdk/clients/glue';
+import { Glue } from 'aws-sdk';
+
 import { Logger } from '@aws-lambda-powertools/logger'
 import { LambdaInterface } from '@aws-lambda-powertools/commons';
 
 let logger = new Logger({ serviceName: "s3-terraform-lambda" });
 
+const glue = new Glue();
+
 class IndexLambda implements LambdaInterface {
+
+    
     // Decorate your handler class method
     @logger.injectLambdaContext()
     public async handler(event: S3Event, context: Context): Promise<void> {
         
         logger.info(`Function Executed: ${JSON.stringify(event)}`);
     
-        event.Records.forEach(async element => {
-            logger.info(`S3 Record: ${JSON.stringify(element)}`)
-        });
+        if(event.Records.length > 0){
+            event.Records.forEach(async element => {
+                logger.info(`S3 Record: ${JSON.stringify(element)}`)
+            });
+        }
+
+        const request: StartCrawlerRequest = {
+            Name: process.env.CRAWLER_NAME ?? ""
+        }
+
+        const response = glue.startCrawler(request)
+
+        logger.info(` ${JSON.stringify(response)}`)
     }
 }
 
